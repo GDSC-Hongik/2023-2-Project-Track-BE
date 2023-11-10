592e93f54c80a946fbefb1c1f400c7eed9c296d6
kckc0608 <kckc0608@naver.com>
2023-11-10 14:03:28 +0900

    [Round1][Django CRUD] 2-17. 폼 유효성 검사 추가

--- Round1/django/costory/posts/forms.py
+++ Round1/django/costory/posts/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import Post
+from django.core.exceptions import ValidationError
 
 class PostForm(forms.ModelForm):
   
   class Meta:
     model = Post
     fields = ['title', 'content']
+  
+  def clean_title(self):
+      data = self.cleaned_data["title"]
+      if '*' in data:
+         raise ValidationError('*는 안됩니다.')
+      return data
+  
