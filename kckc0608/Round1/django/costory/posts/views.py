5cdba21e3458d30dca1ed8e354941f6d20a5c111
kckc0608 <kckc0608@naver.com>
2023-11-13 01:35:25 +0900

    [Round1][Django CRUD] 4-12. DeleteView 를 적용하여 포스트 삭제 로직 수정

--- kckc0608/Round1/django/costory/posts/views.py
+++ kckc0608/Round1/django/costory/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse
 from .models import Post
 from .forms import PostForm
@@ -29,18 +29,6 @@ class PostDetailView(DetailView):
   context_object_name = 'post'
 
 
-# def post_update(request, post_id):
-#   post = get_object_or_404(Post, id=post_id)
-
-#   if request.method == 'POST':
-#     post_form = PostForm(request.POST, instance=post) # 새로운 Post 객체를 만드는 게 아니므로, 기존 인스턴스를 넘겨줌.
-#     if post_form.is_valid():
-#       post_form.save()
-#       return redirect('post-detail', post_id = post.id)
-#   else:
-#     post_form = PostForm(instance=post) # 기존 post 데이터 내용을 담아서 폼 생성
-#   return render(request, 'posts/post_form.html', {'form': post_form})
-
 class PostUpdateView(UpdateView):
   model = Post
   form_class = PostForm
@@ -51,13 +39,14 @@ class PostUpdateView(UpdateView):
     return reverse('post-detail', kwargs={'post_id': self.object.id})
 
 
-def post_delete(request, post_id):
-  post = get_object_or_404(Post, id=post_id)
-  if request.method == 'POST':
-    post.delete()
-    return redirect('post-list')
+class PostDeleteView(DeleteView):
+  model = Post
+  template_name = 'posts/post_confirm_delete.html'
+  pk_url_kwarg = 'post_id'
+  context_object_name = 'post'
 
-  return render(request, 'posts/post_confirm_delete.html', {'post':post})
+  def get_success_url(self) -> str:
+    return reverse('post-list')
 
 
 def index(request):
