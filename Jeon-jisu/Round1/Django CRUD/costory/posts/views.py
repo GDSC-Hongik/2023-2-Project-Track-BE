fdaf5036af4e91988f36bdcff119097ddbce7c6c
Jeon-jisu <wltnjeon0119@gmail.com>
2023-11-12 23:45:19 +0900

    [Round1] [Django CRUD] 4-17 Delete overlap contents

--- Jeon-jisu/Round1/Django CRUD/costory/posts/views.py
+++ Jeon-jisu/Round1/Django CRUD/costory/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from django.views.generic import CreateView,ListView,DetailView, UpdateView, DeleteView
+from django.views.generic import (
+    CreateView,ListView,DetailView, UpdateView, DeleteView, RedirectView
+)
 from django.urls import reverse
 from .models import Post
 from .forms import PostForm
@@ -9,16 +11,12 @@ from .forms import PostForm
 # Create your views here.
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'posts/post_detail.html'
-    pk_url_kwarg = 'post_id'
-    context_object_name = 'post'
 
 class PostCreateView(CreateView):
     model = Post 
     form_class = PostForm
-    template_name = "posts/post_form.html"
     def get_success_url(self):
-        return reverse('post-detail',kwargs={'post_id':self.object.id})
+        return reverse('post-detail',kwargs={'pk':self.object.id})
     
 class PostListView(ListView):
     model = Post
@@ -28,18 +26,13 @@ class PostListView(ListView):
 class PostUpdateView(UpdateView):
     model = Post
     form_class = PostForm 
-    template_name = "posts/post_form.html"
-    pk_url_kwarg = 'post_id'
     def get_success_url(self):
-        return reverse('post-detail',kwargs={'post_id':self.object.id})
+        return reverse('post-detail',kwargs={'pk':self.object.id})
 
 class PostDeleteView(DeleteView):
     model = Post
-    template_name = 'posts/post_confirm_delete.html'
-    pk_url_kwarg = 'post_id'
-    context_object_name = 'post'
     def get_success_url(self):
         return reverse('post-list')
 
-def index(request):
-    return redirect('post-list')
+class IndexRedirectView(RedirectView):
+    pattern_name = 'post-list'
