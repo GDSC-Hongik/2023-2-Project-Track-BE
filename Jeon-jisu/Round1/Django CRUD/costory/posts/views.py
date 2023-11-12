a7b0667c8512129e26fe43cf7dbf3e5352133329
Jeon-jisu <wltnjeon0119@gmail.com>
2023-11-12 22:43:20 +0900

    [Round1] [Django CRUD] 4-4 implement generic view

--- Jeon-jisu/Round1/Django CRUD/costory/posts/views.py
+++ Jeon-jisu/Round1/Django CRUD/costory/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from django.views import View
+from django.views.generic import CreateView
+from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
@@ -20,15 +21,12 @@ def post_detail(request, post_id):
     context = {'post': post}
     return render(request, 'posts/post_detail.html', context)
 
-class PostCreateView(View):
-    def get(self, request):
-        post_form = PostForm()
-        return render(request, 'posts/post_form.html', {'form':post_form})
-    def post(self,request):
-        post_form = PostForm(request.POST)
-        if post_form.is_valid():
-            new_post = post_form.save()
-            return redirect('post-detail',post_id=new_post.id)
+class PostCreateView(CreateView):
+    model = Post 
+    form_class = PostForm
+    template_name = "posts/post_form.html"
+    def get_success_url(self):
+        return reverse('post-detail',kwargs={'post_id':self.object.id})
 
 def post_update(request, post_id):
     post = get_object_or_404(Post, id=post_id)
