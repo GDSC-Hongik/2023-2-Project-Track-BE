b1c5f864caf41f99cf9c451fb4f57a903bc2f27e
kckc0608 <kckc0608@naver.com>
2023-11-13 02:37:01 +0900

    [Round1][Django CRUD] 4-17. 기본값을 활용한 코드 리펙토링

--- kckc0608/Round1/django/costory/posts/views.py
+++ kckc0608/Round1/django/costory/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView, RedirectView
 from django.urls import reverse
 from .models import Post
 from .forms import PostForm
@@ -7,47 +7,37 @@ from .forms import PostForm
 class PostCreateView(CreateView):
   model = Post
   form_class = PostForm
-  template_name = 'posts/post_form.html' # context 는 알아서 "form" 이라는 키워드로 넘겨줌
 
   def get_success_url(self) -> str:
-    return reverse('post-detail', kwargs={'post_id': self.object.id}) ## 리다이렉트 주소 및 인자 전달
+    return reverse('post-detail', kwargs={'pk': self.object.id}) ## 리다이렉트 주소 및 인자 전달
 
 
 class PostListView(ListView):
   model = Post
-  template_name = 'posts/post_list.html'
-  context_object_name = 'posts'
   ordering = ['-dt_created'] # 최근 작성순으로 정렬 (날짜 역순)
   paginate_by = 6
-  page_kwarg = 'page'
 
 
 class PostDetailView(DetailView):
   model = Post
-  template_name = 'posts/post_detail.html'
-  pk_url_kwarg = 'post_id'
-  context_object_name = 'post'
 
 
 class PostUpdateView(UpdateView):
   model = Post
   form_class = PostForm
-  template_name = 'posts/post_form.html'
-  pk_url_kwarg = 'post_id'
   
   def get_success_url(self) -> str:
-    return reverse('post-detail', kwargs={'post_id': self.object.id})
+    return reverse('post-detail', kwargs={'pk': self.object.id})
 
 
 class PostDeleteView(DeleteView):
   model = Post
-  template_name = 'posts/post_confirm_delete.html'
-  pk_url_kwarg = 'post_id'
-  context_object_name = 'post'
 
   def get_success_url(self) -> str:
     return reverse('post-list')
 
 
-def index(request):
-  return redirect('post-list')
+# def index(request):
+#   return redirect('post-list')
+class IndexRedirectView(RedirectView):
+  pattern_name = 'post-list'
