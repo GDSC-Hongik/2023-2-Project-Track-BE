7625295b99b42487702347bb631aa9ce6c9ac27f
jaychung0101 <jihoon20130@gmail.com>
2023-11-13 03:30:58 +0900

    [Round1] B. 감정일기 ~2-17

--- Round1/B. Django CRUD/mindnote/diary/models.py
+++ Round1/B. Django CRUD/mindnote/diary/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from .validators import validate_no_hash, validate_no_numbers, validate_score
 
 # Create your models here.
 class Page(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, validators=[validate_no_hash])
     content = models.TextField()
-    feeling = models.CharField(max_length=80)
-    score = models.IntegerField()
+    feeling = models.CharField(max_length=80, validators=[validate_no_hash, validate_no_numbers])
+    score = models.IntegerField(validators=[validate_score])
     dt_created = models.DateField()
 
     def __str__(self):
--- Round1/B. Django CRUD/mindnote/diary/views.py
+++ Round1/B. Django CRUD/mindnote/diary/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Page
+from .forms import PageForm
+
 # Create your views here.
 def page_list(request):
     object_list = Page.objects.all()
@@ -12,3 +14,13 @@ def page_detail(request, page_id):
 
 def info(request):
     return render(request, 'diary/info.html')
+
+def page_create(request):
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            new_page = form.save()
+            return redirect('page-detail', page_id=new_page.id)
+    else:
+        form = PageForm()
+    return render(request, 'diary/page_form.html', {'form' : form})
