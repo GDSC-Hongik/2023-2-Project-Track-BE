028785ae900082358d6f849ceb18b40595240bb8
Jeon-jisu <wltnjeon0119@gmail.com>
2023-11-12 18:03:27 +0900

    [Round1][Django CRUD] Setting up the Main Page URL

--- Jeon-jisu/Round1/Django CRUD/costory/posts/views.py
+++ Jeon-jisu/Round1/Django CRUD/costory/posts/views.py
@@ -41,3 +41,6 @@ def post_delete(request, post_id):
         return redirect('post-list')
     else:
         return render(request, 'posts/post_confirm_delete.html', {'post':post})
+
+def index(request):
+    return redirect('post-list')
