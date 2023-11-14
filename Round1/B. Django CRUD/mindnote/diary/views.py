4ccc8e073e361920c86776e37343e63a014010b3
jaychung0101 <jihoon20130@gmail.com>
2023-11-15 04:34:15 +0900

    [Round1] B. 감정일기 ~3장

--- Round1/B. Django CRUD/mindnote/diary/views.py
+++ Round1/B. Django CRUD/mindnote/diary/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Page
 from .forms import PageForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def page_list(request):
     object_list = Page.objects.all()
-    return render(request, 'diary/page_list.html', {'object_list':object_list})
+    paginator = Paginator(object_list, 8)
+    curr_page_number = request.GET.get('page')
+    if curr_page_number is None:
+        curr_page_number = 1
+    page = paginator.page(curr_page_number)
+    return render(request, 'diary/page_list.html', {'page': page})
 
 def page_detail(request, page_id):
     object = Page.objects.get(id=page_id)
@@ -24,3 +30,25 @@ def page_create(request):
     else:
         form = PageForm()
     return render(request, 'diary/page_form.html', {'form' : form})
+
+def page_update(request, page_id):
+    object = Page.objects.get(id=page_id)
+    if request.method == 'POST':
+        form = PageForm(request.POST, instance=object)
+        if form.is_valid():
+            form.save()
+            return redirect('page-detail', page_id=object.id)
+    else:
+        form = PageForm(instance=object)
+    return render(request, "diary/page_form.html", {'form': form})
+
+def page_delete(request, page_id):
+    object = Page.objects.get(id=page_id)
+    if request.method == 'POST':
+        object.delete()
+        return redirect('page-list')
+    else:
+        return render(request, 'diary/page_confirm_delete.html', {'object': object})
+    
+def index(request):
+    return render(request, 'diary/index.html')
