3b863a692f543376c0cdc70fbb8d5b780ab471a2
sleepyhoon <101882530+sleepyhoon@users.noreply.github.com>
2023-11-15 01:36:16 +0900

    [Round1] 4장 까지 정리 완료 기간외 제출

---
+++ ROUND1/Django CRUD/4장 클래스형 뷰/validators.py
@@ -0,0 +1,6 @@
+from django.core.exceptions import ValidationError
+
+def validate_symbols(value):
+    if ('@'in value) or ('#'in value) or ('&'in value):
+        raise ValidationError('"@"와 "#"와 "&"은 사용 불가입니다.',code='symbol-err')
+    
