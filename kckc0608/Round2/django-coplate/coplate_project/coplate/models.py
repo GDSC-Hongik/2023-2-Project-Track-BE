c3b1eeb120312f0df9da9a8dc9539daca748390e
kckc0608 <kckc0608@naver.com>
2023-11-17 00:00:52 +0900

    [Round2][Django User] 1-17. 닉네임 필드 설정하고 폼에 추가하기

--- kckc0608/Round2/django-coplate/coplate_project/coplate/models.py
+++ kckc0608/Round2/django-coplate/coplate_project/coplate/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+  nickname = models.CharField(max_length=15, unique=True, null=True)
   def __str__(self):
       return self.email
   
