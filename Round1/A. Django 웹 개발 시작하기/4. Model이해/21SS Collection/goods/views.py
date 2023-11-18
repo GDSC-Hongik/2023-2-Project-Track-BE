96d88ef45b253394e3ce3a9f0937812fb63b387d
jaychung0101 <jihoon20130@gmail.com>
2023-11-19 00:31:21 +0900

    Changing Files

--- Round1/A. Django 웹 개발 시작하기/4. Model이해/21SS Collection/goods/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from goods.models import Product
-
-# Create your views here.
-def index(request):
-    context = dict()
-    goods = Product.objects.all()
-    context["goods"] = goods
-    return render(request, 'goods/index.html', context=context)
