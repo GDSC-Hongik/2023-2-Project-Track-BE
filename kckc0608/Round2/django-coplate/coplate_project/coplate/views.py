4330b47acc5e35aaebd331eac441e053797f72a0
kckc0608 <kckc0608@gmail.com>
2023-11-19 11:56:41 +0900

    [Round2][Django User] 3-10. 인덱스 뷰 설정

--- kckc0608/Round2/django-coplate/coplate_project/coplate/views.py
+++ kckc0608/Round2/django-coplate/coplate_project/coplate/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.generic import ListView
+from .models import Review
 from allauth.account.views import PasswordChangeView
 
 # Create your views here.
 def index(request):
   print(request.user.is_authenticated)
   return render(request, "coplate/index.html")
+
+
+class IndexView(ListView):
+  model = Review
+  template_name = "coplate/index.html"
+  context_object_name = "reviews"
+  paginate_by = 4
+  ordering = ["-dt_created"]
+   
 
 
 class CustomPasswordChangeView(PasswordChangeView):
