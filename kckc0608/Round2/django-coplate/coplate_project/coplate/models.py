c772cf95bca01522263ce7b6f0823b5333b9be4e
kckc0608 <kckc0608@gmail.com>
2023-11-19 10:13:45 +0900

    [Round2][Django User] 3-04. 리뷰 모델 작성

--- kckc0608/Round2/django-coplate/coplate_project/coplate/models.py
+++ kckc0608/Round2/django-coplate/coplate_project/coplate/models.py
@@ -15,26 +15,25 @@ class User(AbstractUser):
       return self.email
   
   
-  class Review(models.Model):
-    title = models.CharField(max_length=30)
-    restaurant_name = models.CharField(max_length=20)
-    restaurant_link = models.URLField(
-      validators=[validate_restaurant_link]
-    )
-    
-    RATING_CHOICES = [
-      (1, 1),
-      (2, 2),
-      (3, 3),
-      (4, 4),
-      (5, 5),
-    ]
-    raiting = models.IntegerField(choices=RATING_CHOICES)
-    
-    image1 = models.ImageField()
-    image2 = models.ImageField()
-    image3 = models.ImageField()
-    content = models.TextField()
-    dt_created = models.DateTimeField(auto_now_add=True)
-    dt_updated = models.DateTimeField(auto_now=True)
+class Review(models.Model):
+  title = models.CharField(max_length=30)
+  restaurant_name = models.CharField(max_length=20)
+  restaurant_link = models.URLField(
+    validators=[validate_restaurant_link]
+  )
   
+  RATING_CHOICES = [
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+  ]
+  raiting = models.IntegerField(choices=RATING_CHOICES)
+  
+  image1 = models.ImageField(upload_to="review_pics")
+  image2 = models.ImageField(upload_to="review_pics", blank=True)
+  image3 = models.ImageField(upload_to="review_pics", blank=True)
+  content = models.TextField()
+  dt_created = models.DateTimeField(auto_now_add=True)
+  dt_updated = models.DateTimeField(auto_now=True)
