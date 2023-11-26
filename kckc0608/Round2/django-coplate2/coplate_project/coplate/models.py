1f492e6be22eee9eb1567d5f9611a1aa9f9c7e79
kckc0608 <kckc0608@gmail.com>
2023-11-26 20:34:36 +0900

    [Round2][Django Model] 3-03. 댓글 모델 작성, 일대다 관계 설정

--- kckc0608/Round2/django-coplate2/coplate_project/coplate/models.py
+++ kckc0608/Round2/django-coplate2/coplate_project/coplate/models.py
@@ -53,3 +53,18 @@ class Review(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    content = models.TextField()
+    
+    dt_created = models.DateTimeField(auto_now_add=True)
+    
+    dt_modified = models.DateTimeField(auto_now=True)
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.content[:30]
