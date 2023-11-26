9ac8ce1b926346a77ff1de56557aa9360bf65cca
kckc0608 <kckc0608@gmail.com>
2023-11-26 23:18:27 +0900

    [Round2][Django Model] 3-16. 어드민 페이지에서 리뷰에 댓글과 좋아요를 달 수 있도록 인라인 추가

--- kckc0608/Round2/django-coplate2/coplate_project/coplate/admin.py
+++ kckc0608/Round2/django-coplate2/coplate_project/coplate/admin.py
@@ -1,13 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
+from django.contrib.contenttypes.admin import GenericStackedInline
 from .models import User, Review, Comment, Like
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    
+class LikeInline(GenericStackedInline):
+    model = Like
+
+# 리뷰 admin 페이지에서 댓글을 사용하기 위해, Comment Inline 클래스를 만들어 넘겨줌.
+class ReviewAdmin(admin.ModelAdmin):
+    inlines = (
+        CommentInline,
+        LikeInline,
+    )
+
+class CommentAdmin(admin.ModelAdmin):
+    inlines = (
+        LikeInline,
+    )
 
 UserAdmin.fieldsets += ('Custom fields', {'fields': ('nickname', 'profile_pic', 'intro', 'following',)}),
 
 admin.site.register(User, UserAdmin)
 
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
 
 admin.site.register(Comment)
 
