5354b41b77d7be54119bd7d4498616c2667b4623
kckc0608 <kckc0608@naver.com>
2023-11-29 09:59:15 +0900

    [Round2][Django Model] 4-29. 댓글/리뷰 좋아요 여부 시각화

--- kckc0608/Round2/django-coplate2/coplate_project/coplate/views.py
+++ kckc0608/Round2/django-coplate2/coplate_project/coplate/views.py
@@ -41,6 +41,12 @@ class ReviewDetailView(DetailView):
         context["form"] = CommentForm()
         context["review_ctype_id"] = ContentType.objects.get(model='review').id
         context["comment_ctype_id"] = ContentType.objects.get(model='comment').id
+
+        user = self.request.user
+        if user.is_authenticated:
+            review = self.object
+            context["likes_review"] = Like.objects.filter(user=user, review=review).exists()
+            context["liked_comments"] = Comment.objects.filter(review=review).filter(likes__user=user)
         return context
     
 
