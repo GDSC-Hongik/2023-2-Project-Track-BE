e9df5c047ec3d2fbd9c841e6da8db6b051c2d07d
jaychung0101 <jihoon20130@gmail.com>
2023-12-01 19:55:12 +0900

    [Round2] D. podomarket ~3-11

--- jaychung0101/Round2/D. Django 모델 파헤치기/django-coplate2/podomarket_project/podomarket/models.py
+++ jaychung0101/Round2/D. Django 모델 파헤치기/django-coplate2/podomarket_project/podomarket/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.validators import MinValueValidator
 from django.db import models
 
@@ -81,3 +83,18 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content[:30]
+    
+
+class Like(models.Model):
+    dt_created = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+
+    object_id = models.PositiveIntegerField()
+
+    liked_object = GenericForeignKey()
+
+    def __str__(self):
+        return f"({self.user}, {self.liked_object})"
