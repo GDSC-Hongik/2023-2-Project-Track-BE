997c53c2bcd52ccd1fa4d4db9cb56058f95d9e22
Jeon-jisu <wltnjeon0119@gmail.com>
2023-12-21 02:52:44 +0900

    [Round2][Django User] all. code update

--- Jeon-jisu/Round2/django-coplate/coplate_project/coplate_project/settings.py
+++ Jeon-jisu/Round2/django-coplate/coplate_project/coplate_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v1p-5=6*+c9w*hyca(az^t!*s95(v(wo*!&*5pgpu$kdxcurg_'
+SECRET_KEY = '4l&q&ndl1!&n_o=!s@xup89ksqlo7(=w3nz7dkxk+up_g6f-d_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'coplate',
     'widget_tweaks',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    "coplate.middleware.ProfileSetupMiddleware"
 ]
 
 ROOT_URLCONF = 'coplate_project.urls'
@@ -92,7 +96,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "coplate.validators.CustomPasswordValidator"
+        'NAME': 'coplate.validators.CustomPasswordValidator',
     }
 ]
 
@@ -115,10 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/uploads/"
 
-# Auth Settings
+# Auth Setting
 
-AUTH_USER_MODEL = 'coplate.User'
+AUTH_USER_MODEL = "coplate.User"
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
@@ -127,22 +133,21 @@ AUTHENTICATION_BACKENDS = [
     # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-
-ACCOUNT_SIGNUP_REDIRECT_URL = "index"
+ACCOUNT_SIGNUP_REDIRECT_URL = "profile-set"
 LOGIN_REDIRECT_URL = "index"
+LOGIN_URL = "account_login"
 ACCOUNT_LOGOUT_ON_GET = True
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
-ACCOUNT_SIGNUP_FORM_CLASS = "coplate.forms.SignupForm"
+# ACCOUNT_SIGNUP_FORM_CLASS = "coplate.forms.SignupForm"
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = True
-# ACCOUNT_EMAIL_VARIFICATION = "optional" #default
+ACCOUNT_EMAIL_VARIFICATION = "optional"
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = "account_email_confirmation_done"
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = "account_email_confirmation_done"
-# PASSWORD_RESET_TIMEOUT_DAYS = 3
-# SESSION_COOKIE_AGE = 3600
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 # Email settings
 
-EMAIL_BACKEND =  "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- Jeon-jisu/Round2/django-coplate/coplate_project/coplate_project/urls.py
+++ Jeon-jisu/Round2/django-coplate/coplate_project/coplate_project/urls.py
@@ -16,18 +16,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 from coplate.views import CustomPasswordChangeView
 
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
-    # coplate
-    path('', include("coplate.urls")),
+    # coplate 
+    path('', include('coplate.urls')),
+    path(
+        "email-confirmation-required/",
+        TemplateView.as_view(template_name='account/email_confirmation_required.html'),
+        name="account_email_confirmation_required",
+    ),
     # allauth
-    path('email-confirmation-done', 
-         TemplateView.as_view(
-            template_name = "coplate/email_confirmation_done.html") ,
-            name = "account_email_confirmation_done"),
-    path('password/change/',CustomPasswordChangeView.as_view(), name="account_password_change"),
+    path(
+        "email-confirmation-done/",
+        TemplateView.as_view(template_name='account/email_confirmation_done.html'),
+        name="account_email_confirmation_done",
+    ),
+    path("password/change/",CustomPasswordChangeView.as_view(), name = "account_password_change"),
     path('', include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
