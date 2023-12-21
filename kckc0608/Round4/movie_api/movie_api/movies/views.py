d845f1946f7cb43745c2e4e793407f0655511dca
kckc0608 <kckc0608@gmail.com>
2023-12-21 22:34:54 +0900

    [Round4][REST API] 2-01. 영화 리스트 조회 api 추가

--- kckc0608/Round4/movie_api/movie_api/movies/views.py
+++ kckc0608/Round4/movie_api/movie_api/movies/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Movie
+from .serializer import MovieSerializer
+
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True) # 여러 데이터 리스트를 직렬화 하는 경우
+    return Response(serializer.data, status=200)
