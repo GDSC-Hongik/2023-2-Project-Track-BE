fc9666e6dee5cd51861f72d7f9210526200bf316
kckc0608 <kckc0608@gmail.com>
2023-12-21 22:46:00 +0900

    [Round4][REST API] 2-02. 영화 데이터 생성을 위한 시리얼라이저 수정

--- kckc0608/Round4/movie_api/movie_api/movies/serializer.py
+++ kckc0608/Round4/movie_api/movie_api/movies/serializer.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Movie
 
 class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     opening_date = serializers.DateField()
     running_time = serializers.IntegerField()
     overview = serializers.CharField()
     
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
