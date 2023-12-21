cfb6d26124982694f5edeef90c7d857797cde2e6
kckc0608 <kckc0608@gmail.com>
2023-12-21 22:51:39 +0900

    [Round4][REST API] 2-04. 영화 데이터 생성 뷰 추가

--- kckc0608/Round4/movie_api/movie_api/movies/views.py
+++ kckc0608/Round4/movie_api/movie_api/movies/views.py
@@ -3,12 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Movie
 from .serializer import MovieSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True) # 여러 데이터 리스트를 직렬화 하는 경우
-    return Response(serializer.data, status=200)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True) # 여러 데이터 리스트를 직렬화 하는 경우
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        data = request.data
+        serializer = MovieSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
