2ed78a5fee83c72cd0b47ecf5e4c80e0a39bb02a
kckc0608 <kckc0608@gmail.com>
2023-12-22 11:54:25 +0900

    [Round4][REST API] 2-15. name 유일성 유효성 검사 추가

--- kckc0608/Round4/movie_api/movie_api/movies/serializer.py
+++ kckc0608/Round4/movie_api/movie_api/movies/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Movie
 from django.core.validators import MaxLengthValidator, MinLengthValidator
+from rest_framework.validators import UniqueValidator
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +9,7 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'opening_date', 'running_time', 'overview']
     
     overview = serializers.CharField(validators=[MinLengthValidator(limit_value=10), MaxLengthValidator(limit_value=300)])
+    name = serializers.CharField(validators=[UniqueValidator(
+        queryset=Movie.objects.all(),
+        message="이미 존재하는 영화 이름입니다.",
+    )])
