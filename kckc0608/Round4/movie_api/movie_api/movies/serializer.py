4975b6f6faacd3df04c6980e12b39e4e22951df0
kckc0608 <kckc0608@gmail.com>
2023-12-22 11:55:26 +0900

    [Round4][REST API] 2-15. name, overview 조합의 유일성 유효성 검사로 변경

--- kckc0608/Round4/movie_api/movie_api/movies/serializer.py
+++ kckc0608/Round4/movie_api/movie_api/movies/serializer.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
 from .models import Movie
 from django.core.validators import MaxLengthValidator, MinLengthValidator
-from rest_framework.validators import UniqueValidator
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['id', 'name', 'opening_date', 'running_time', 'overview']
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Movie.objects.all(),
+                fields=['name', 'overview']
+            )
+        ]
     
     overview = serializers.CharField(validators=[MinLengthValidator(limit_value=10), MaxLengthValidator(limit_value=300)])
-    name = serializers.CharField(validators=[UniqueValidator(
-        queryset=Movie.objects.all(),
-        message="이미 존재하는 영화 이름입니다.",
-    )])
+    # name = serializers.CharField(validators=[UniqueValidator(
+    #     queryset=Movie.objects.all(),
+    #     message="이미 존재하는 영화 이름입니다.",
+    # )])
