0526240966d5d11322fc0ef9979ce18c5bb2636b
kckc0608 <kckc0608@gmail.com>
2023-12-22 11:52:13 +0900

    [Round4][REST API] 2-15. overview 길이 유효성 검사 추가

--- kckc0608/Round4/movie_api/movie_api/movies/serializer.py
+++ kckc0608/Round4/movie_api/movie_api/movies/serializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Movie
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['id', 'name', 'opening_date', 'running_time', 'overview']
+    
+    overview = serializers.CharField(validators=[MinLengthValidator(limit_value=10), MaxLengthValidator(limit_value=300)])
