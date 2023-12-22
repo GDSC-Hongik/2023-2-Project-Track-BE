20efa8bf0e2a7dc9719eb7ba1382aebb1d4d889b
kckc0608 <kckc0608@gmail.com>
2023-12-22 11:57:51 +0900

    [Round4][REST API] 2-14. overview 길이 제한 유효성검사 코드 개선

--- kckc0608/Round4/movie_api/movie_api/movies/serializer.py
+++ kckc0608/Round4/movie_api/movie_api/movies/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Movie
 from django.core.validators import MaxLengthValidator, MinLengthValidator
 from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+from rest_framework.serializers import ValidationError
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,7 +15,14 @@ class MovieSerializer(serializers.ModelSerializer):
             )
         ]
     
-    overview = serializers.CharField(validators=[MinLengthValidator(limit_value=10), MaxLengthValidator(limit_value=300)])
+    # overview = serializers.CharField(validators=[MinLengthValidator(limit_value=10), MaxLengthValidator(limit_value=300)])
+    
+    def validate_overview(self, value):
+        if 10 <= len(value) <= 300:
+            return value
+        raise ValidationError('영화 소개는 10~300자 사이로 입력해주세요.')
+    
+    
     # name = serializers.CharField(validators=[UniqueValidator(
     #     queryset=Movie.objects.all(),
     #     message="이미 존재하는 영화 이름입니다.",
