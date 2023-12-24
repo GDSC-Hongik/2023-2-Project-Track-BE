963dfbcf77775504cc6ae8c8bca5e73206481386
kckc0608 <kckc0608@gmail.com>
2023-12-24 17:23:53 +0900

    [Round4][REST API] 4-01. cors header 설정

--- kckc0608/Round4/movie_api/movie_api/movie_api/settings.py
+++ kckc0608/Round4/movie_api/movie_api/movie_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'movies',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'movie_api.urls'
@@ -128,3 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
