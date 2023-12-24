540eba460949fd9ba7a4d784b63aa530458538ee
kckc0608 <kckc0608@gmail.com>
2023-12-24 16:30:38 +0900

    [Round4][REST API] 2-18. Nested Serializer 사용

--- kckc0608/Round4/movie_api/movie_api/movies/serializer.py
+++ kckc0608/Round4/movie_api/movie_api/movies/serializer.py
@@ -4,9 +4,21 @@ from django.core.validators import MaxLengthValidator, MinLengthValidator
 from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 from rest_framework.serializers import ValidationError
 
+class ReviewSerializer(serializers.ModelSerializer):
+    movie = serializers.StringRelatedField()
+    
+    class Meta:
+        model = Review
+        fields = ['id', 'movie', 'username', 'star', 'comment', 'created']
+        extra_kwargs = {
+            'movie': {'read_only': True},
+        }
+
 class MovieSerializer(serializers.ModelSerializer):
     # movie_reviews = serializers.PrimaryKeyRelatedField(source='reviews', many=True, read_only=True)
-    reviews = serializers.StringRelatedField(many=True)
+    # reviews = serializers.StringRelatedField(many=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Movie
         fields = ['id', 'name', 'opening_date', 'running_time', 'overview', 'reviews']
@@ -30,14 +42,3 @@ class MovieSerializer(serializers.ModelSerializer):
     #     queryset=Movie.objects.all(),
     #     message="이미 존재하는 영화 이름입니다.",
     # )])
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    movie = serializers.StringRelatedField()
-    
-    class Meta:
-        model = Review
-        fields = ['id', 'movie', 'username', 'star', 'comment', 'created']
-        extra_kwargs = {
-            'movie': {'read_only': True},
-        }
