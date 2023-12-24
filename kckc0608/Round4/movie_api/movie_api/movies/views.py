99688ace171cc5247feaacacfb0386aa43f24991
kckc0608 <kckc0608@gmail.com>
2023-12-24 17:15:56 +0900

    [Round4][REST API] 3-07. movie detail view 에 Generic  API View 사용

--- kckc0608/Round4/movie_api/movie_api/movies/views.py
+++ kckc0608/Round4/movie_api/movie_api/movies/views.py
@@ -1,29 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Movie, Review
 from .serializer import MovieSerializer, ReviewSerializer
-
-@api_view(['GET', 'POST'])
-def review_list(request, pk):
-    movie = get_object_or_404(Movie, pk=pk)
-    if request.method == 'GET':
-        reviews = Review.objects.filter(movie=movie)
-        serializer = ReviewSerializer(reviews, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        serializer = ReviewSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(movie=movie)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ReviewList(ListCreateAPIView):
     serializer_class = ReviewSerializer
@@ -40,25 +22,6 @@ class MovieList(ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-class MovieDetail(APIView):
-    def get_object(self, pk):
-        movie = get_object_or_404(Movie, pk=pk)
-        return movie
-    
-    def get(self, request, pk):
-        movie = self.get_object(pk)
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data)
-    
-    def patch(self, request, pk):
-        movie = self.get_object(pk)
-        serializer = MovieSerializer(movie, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        movie = self.get_object(pk)
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class MovieDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
