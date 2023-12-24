861744523a33c918e8916f4318617c88c82f00e3
kckc0608 <kckc0608@gmail.com>
2023-12-24 17:12:49 +0900

    [Round4][REST API] 3-06. Generic  API View 사용

--- kckc0608/Round4/movie_api/movie_api/movies/urls.py
+++ kckc0608/Round4/movie_api/movie_api/movies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import movie_list, movie_detail, review_list, MovieList, MovieDetail
+from .views import MovieList, MovieDetail, ReviewList
 
 urlpatterns = [
     path('movies', MovieList.as_view()),
     path('movies/<int:pk>', MovieDetail.as_view()),
-    path('movies/<int:pk>/reviews', review_list),
+    path('movies/<int:pk>/reviews', ReviewList.as_view()),
 ]
--- kckc0608/Round4/movie_api/movie_api/movies/views.py
+++ kckc0608/Round4/movie_api/movie_api/movies/views.py
@@ -6,42 +6,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 
 from .models import Movie, Review
 from .serializer import MovieSerializer, ReviewSerializer
-
-@api_view(['GET', 'POST'])
-def movie_list(request):
-    if request.method == 'GET':
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True) # 여러 데이터 리스트를 직렬화 하는 경우
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    elif request.method == 'POST':
-        data = request.data
-        serializer = MovieSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer._errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PATCH', 'DELETE'])
-def movie_detail(request, pk):
-    movie = get_object_or_404(Movie, pk=pk)
-    if request.method == 'GET':
-        serializer = MovieSerializer(movie)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    elif request.method == 'PATCH':
-        serializer = MovieSerializer(movie, data=request.data, partial=True) # partial is for 'patch' not 'put'
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def review_list(request, pk):
@@ -57,18 +25,20 @@ def review_list(request, pk):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class MovieList(APIView):
-    def get(self, request):
-        movies = Movie.objects.all()
-        serializers = MovieSerializer(movies, many=True)
-        return Response(serializers.data)
+class ReviewList(ListCreateAPIView):
+    serializer_class = ReviewSerializer
     
-    def post(self, request):
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        movie = get_object_or_404(Movie, pk=self.kwargs.get('pk'))
+        return Review.objects.filter(movie=movie)
+    
+    def perform_create(self, serializer):
+        movie = get_object_or_404(Movie, pk=self.kwargs.get('pk'))
+        serializer.save(movie=movie)
+
+class MovieList(ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
 
 class MovieDetail(APIView):
     def get_object(self, pk):
