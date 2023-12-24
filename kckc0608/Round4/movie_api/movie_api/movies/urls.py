0e852c5a03b950f0ce68395ca5a7d7f408ea6b40
kckc0608 <kckc0608@gmail.com>
2023-12-24 16:45:48 +0900

    [Round4][REST API] 3-02. API View 사용

--- kckc0608/Round4/movie_api/movie_api/movies/urls.py
+++ kckc0608/Round4/movie_api/movie_api/movies/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import movie_list, movie_detail, review_list
+from .views import movie_list, movie_detail, review_list, MovieList
 
 urlpatterns = [
-    path('movies', movie_list),
+    path('movies', MovieList.as_view()),
     path('movies/<int:pk>', movie_detail),
     path('movies/<int:pk>/reviews', review_list),
 ]
--- kckc0608/Round4/movie_api/movie_api/movies/views.py
+++ kckc0608/Round4/movie_api/movie_api/movies/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
 
 from .models import Movie, Review
 from .serializer import MovieSerializer, ReviewSerializer
@@ -53,5 +54,18 @@ def review_list(request, pk):
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(movie=movie)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class MovieList(APIView):
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializers = MovieSerializer(movies, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
